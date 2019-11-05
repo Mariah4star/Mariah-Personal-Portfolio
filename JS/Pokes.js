9953077cbcfd48bf406113e824f3dc9b89e140c1
Mariah4star <mya.foerster@gmail.com>
2019-11-05 12:10:05 -0700

    Got Back working

--- JS/Pokes.js
+++ JS/Pokes.js
@@ -29,20 +29,29 @@ function populateDOM(single_pokemon) {
     let pokeBack = document.createElement('div')
     let name = document.createElement('h1')
     let pic = document.createElement('img')
+    let picture = document.createElement('img')
     
     pokeScene.setAttribute('class', 'scene')
     pokeCard.setAttribute('class', 'card')
     pokeFront.setAttribute('class', 'charDiv card__face card__face--front')
     pokeBack.setAttribute('class', 'card__face card__face--back')
     pic.setAttribute('class', 'picDiv')
+    picture.setAttribute('class', 'pictureDiv')
+
+    pokeCard.addEventListener('click', function() {
+        pokeCard.classList.toggle('is-flipped');
+      });
 
    let pokeNum = getPokeNumber(single_pokemon.id)
 
     name.textContent = `${single_pokemon.name} height: ${single_pokemon.height}`
     pic.src = `../Images/${pokeNum}.png`
+    picture.src = "../Images/pokecardBack.png"
 
     pokeFront.appendChild(pic)
     pokeFront.appendChild(name)
+
+    pokeBack.appendChild(picture)
 
     pokeCard.appendChild(pokeFront)
     pokeCard.appendChild(pokeBack)
@@ -50,9 +59,6 @@ function populateDOM(single_pokemon) {
 
     mainArea.appendChild(pokeScene)
 
-    pokeCard.addEventListener( 'click', function() {
-        pokeCard.classList.toggle('is-flipped');
-      });
 } 
 
 

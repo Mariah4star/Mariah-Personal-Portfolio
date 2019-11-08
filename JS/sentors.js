4161be33b0b15f3cbd3b26c28528c9489b0b410b
Mariah4star <mya.foerster@gmail.com>
2019-11-07 21:19:53 -0700

    Got senators working

--- JS/sentors.js
+++ JS/sentors.js
@@ -9,6 +9,7 @@ async function getAPIData(url) {
 }
 
 let allSenators = []
+
 const theData = getAPIData('senators.json').then(data => {
  allSenators = data.results[0].members
  populateDOM(allSenators)
@@ -16,21 +17,65 @@ const theData = getAPIData('senators.json').then(data => {
 
 const container = document.querySelector('.container')
 
-function populateDOM(senator_array) {
-senator_array.forEach(senator => {
+function populateDOM(senator_arr) {
+senator_arr.forEach(senator => {
  let card = document.createElement('div')
  card.setAttribute('class', 'card')
+
  let cardImage = document.createElement('div')
  cardImage.setAttribute('class', 'card-image')
+
  let figure = document.createElement('figure')
  figure.setAttribute('class', 'image is-4by3')
+
  let figureImage = document.createElement('img')
- figureImage.src = 'https://bulma.io/images/placeholders/1280x960.png'
+ figureImage.src = `https://www.congress.gov/img/member/${senator.id.toLowerCase()}_200.jpg`
+
  figureImage.alt = 'Placeholder image'
 
  figure.appendChild(figureImage)
  cardImage.appendChild(figure)
  card.appendChild(cardImage)
+ card.appendChild(cardContent(senator))
  container.appendChild(card)
-});
+})
+}
+
+function cardContent(senator) {
+    let cardContent = document.createElement('div')
+    cardContent.setAttribute('class', 'card-content')
+
+    let media = document.createElement('div')
+    media.setAttribute('class', 'media')
+
+    let mediaLeft = document.createElement('div')
+    mediaLeft.setAttribute('class', 'media-left')
+
+    let figure = document.createElement('figure')
+    figure.setAttribute('class', 'image is-48x48')
+
+    let img = document.createElement('img')
+    img.src = ``
+    img.alt = 'Placeholder image'
+
+    let mediaContent = document.createElement('div')
+    mediaContent.setAttribute('class', 'media-content')
+
+    let titleP = document.createElement('p')
+    titleP.setAttribute('class', 'title is-4')
+    titleP.textContent = 'foo'
+    
+    let subtitleP = document.createElement('p')
+    subtitleP.setAttribute('class', 'subtitle is-6')
+    subtitleP.textContent = 'bar'
+
+    mediaContent.appendChild(titleP)
+    mediaContent.appendChild(subtitleP)
+    figure.appendChild(img)
+    mediaLeft.appendChild(figure)
+    media.appendChild(mediaLeft)
+    media.appendChild(mediaContent)
+    cardContent.appendChild(media)
+
+    return cardContent
 }

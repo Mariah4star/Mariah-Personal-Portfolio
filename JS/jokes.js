14122730ceb488ae25f35f886173eafbe4ead65d
Mariah4star <mya.foerster@gmail.com>
2019-11-23 13:34:12 -0700

    getting it up and going

---
+++ JS/jokes.js
@@ -0,0 +1,19 @@
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const theData = getAPIData('https://official-joke-api.appspot.com/random_ten')
+    .then(data => {
+        for (const joke of data.results) {
+            getAPIData(joke.url)
+                .then(jokedata => {
+                    populateDOM(jokedata)
+                })
+        }
+    })
